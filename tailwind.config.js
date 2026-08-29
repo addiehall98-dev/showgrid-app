af77b9e9034f2fa627961d71493772817507b4d3
addiehall98-dev <addie.hall98@gmail.com>
2026-08-28 22:42:44 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        stage: '#0e0f12',
+        accent: '#f59e0b',
+        curtain: '#ef4444'
+      }
+    }
+  },
+  plugins: [],
+};
